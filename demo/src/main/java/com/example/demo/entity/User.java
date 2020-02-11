841a8ab941b641b21292e9ba69c95bbd907f9222
Vasyl Petryshak <vasyl.petryshak@globaltechmakers.com>
2020-02-11 19:42:14 +0200

    add email

--- demo/src/main/java/com/example/demo/entity/User.java
+++ demo/src/main/java/com/example/demo/entity/User.java
@@ -4,7 +4,6 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
 
 @Entity
 public class User {
@@ -24,6 +23,8 @@ public class User {
 
     @NotNull
     private String address;
+
+    private String email;
 
     public Integer getId() {
         return id;
@@ -63,5 +64,13 @@ public class User {
 
     public void setAddress(String address) {
         this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 }
